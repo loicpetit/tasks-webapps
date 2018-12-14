d7718c8e8f12ce6577deddb3639d8b0d40f73b81
Loic PETIT <loic.petit@capgemini.com>
2018-12-14 20:02:56 +0100

    Mise en place theme blanc et noir. Il reste à faire les boutons theme et config dans la top bar.

--- docker/Start-Container.ps1
+++ docker/Start-Container.ps1
@@ -1,2 +1,3 @@
 docker build -t task-webapps ..
-docker run --rm --detach --name task-webapps -d -p 4000:80 task-webapps
+docker run --rm --detach --name task-webapps --publish 4000:80 task-webapps
+#docker run --rm --detach --name task-webapps --publish 4000:80 -v $pwd\..\src\html\:/usr/share/nginx/html/html/ task-webapps
