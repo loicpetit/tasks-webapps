882fda677a58f4fc6657ce4f8a3e79af6cfab543
Loic PETIT <loic.petit@capgemini.com>
2018-12-17 17:53:53 +0100

    Mount src when running container.

--- docker/Start-Container.ps1
+++ docker/Start-Container.ps1
@@ -1,3 +1,2 @@
 docker build -t task-webapps ..
-docker run --rm --detach --name task-webapps --publish 4000:80 task-webapps
-#docker run --rm --detach --name task-webapps --publish 4000:80 -v $pwd\..\src\html\:/usr/share/nginx/html/html/ task-webapps
+docker run --rm --detach --name task-webapps --publish 4000:80 -v $pwd\..\src\html\:/usr/share/nginx/html/html/ task-webapps
